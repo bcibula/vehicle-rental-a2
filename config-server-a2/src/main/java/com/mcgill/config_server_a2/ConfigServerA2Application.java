4de40251237a2aafd4639949d5b1c97c484c76e9
Brad Cibula <40251651+bcibula@users.noreply.github.com>
2026-07-30 22:37:42 -0400

    Update ConfigServerA2Application.java
    added comments

--- config-server-a2/src/main/java/com/mcgill/config_server_a2/ConfigServerA2Application.java
+++ config-server-a2/src/main/java/com/mcgill/config_server_a2/ConfigServerA2Application.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+// Tells Spring this app is a Config Server, which serves settings to other apps.
 @EnableConfigServer
+// Tells Spring Boot to auto-set up the app and start everything it needs.
 @SpringBootApplication
 public class ConfigServerA2Application {
 
+	// This is the main starting point when you run the program.
 	public static void main(String[] args) {
+		// Starts the Spring Boot application.
 		SpringApplication.run(ConfigServerA2Application.class, args);
 	}
 
